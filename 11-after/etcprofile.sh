aab574ec865d03dce47f35032c66e07bc2871c63
TolaMironcenko <mironcenkotola@gmail.com>
2024-10-17 20:04:19 +0700

    etc/profile

---
+++ 11-after/etcprofile.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+cat > /etc/profile << "EOF"
+# /etc/profile
+
+# Append "$1" to $PATH when not already in.
+# This function API is accessible to scripts in /etc/profile.d
+append_path () {
+    case ":$PATH:" in
+        *:"$1":*)
+            ;;
+        *)
+            PATH="${PATH:+$PATH:}$1"
+    esac
+}
+
+# Append our default paths
+append_path '/usr/local/sbin'
+append_path '/usr/local/bin'
+append_path '/usr/bin'
+
+# Force PATH to be environment
+export PATH
+
+# Load profiles from /etc/profile.d
+if test -d /etc/profile.d/; then
+	for profile in /etc/profile.d/*.sh; do
+		test -r "$profile" && . "$profile"
+	done
+	unset profile
+fi
+
+# Unload our profile API functions
+unset -f append_path
+
+# Source global bash config, when interactive but not posix or sh mode
+if test "$BASH" &&\
+   test "$PS1" &&\
+   test -z "$POSIXLY_CORRECT" &&\
+   test "${0#-}" != sh &&\
+   test -r /etc/bashrc
+then
+	. /etc/bashrc
+fi
+
+# Termcap is outdated, old, and crusty, kill it.
+unset TERMCAP
+
+# Man is much better than us at figuring this out
+unset MANPATH
+EOF
+
+install --directory --mode=0755 --owner=root --group=root /etc/profile.dinstall --directory --mode=0755 --owner=root --group=root /etc/profile.d
+
+cat > /etc/profile.d/locale.sh << "EOF"
+#!/bin/sh
+
+# load locale.conf in XDG paths.
+# /etc/locale.conf loads and overrides by kernel command line is done by systemd
+# But we override it here, see FS#56688
+if [ -z "$LANG" ]; then
+  if [ -n "$XDG_CONFIG_HOME" ] && [ -r "$XDG_CONFIG_HOME/locale.conf" ]; then
+    . "$XDG_CONFIG_HOME/locale.conf"
+  elif [ -n "$HOME" ] && [ -r "$HOME/.config/locale.conf" ]; then
+    . "$HOME/.config/locale.conf"
+  elif [ -r /etc/locale.conf ]; then
+    . /etc/locale.conf
+  fi
+fi
+
+# define default LANG to C if not already defined
+LANG=${LANG:-C}
+
+# export all locale (7) variables when they exist
+export LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY \
+       LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT \
+       LC_IDENTIFICATION
+EOF
+
+cat > /etc/bashrc << "EOF"
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+NORMAL="\[\e[0m\]"
+RED="\[\e[1;31m\]"
+GREEN="\[\e[1;32m\]"
+if [[ $EUID == 0 ]] ; then
+  PS1="$RED\u [ $NORMAL\w$RED ]# $NORMAL"
+else
+  PS1="$GREEN\u [ $NORMAL\w$GREEN ]\$ $NORMAL"
+fi
+
+unset RED GREEN NORMAL
+EOF
--- 9-system-configuration/5-locale.sh
+++ 9-system-configuration/5-locale.sh
@@ -4,26 +4,3 @@ cat > /etc/locale.conf << "EOF"
 LC_ALL=en_US.UTF-8
 LANG=en_US.UTF-8
 EOF
-
-cat > /etc/profile << "EOF"
-# Begin /etc/profile
-
-for i in $(locale); do
-  unset ${i%=*}
-done
-
-if [[ "$TERM" = linux ]]; then
-  export LANG=C.UTF-8
-else
-  source /etc/locale.conf
-
-  for i in $(locale); do
-    key=${i%=*}
-    if [[ -v $key ]]; then
-      export $key
-    fi
-  done
-fi
-
-# End /etc/profile
-EOF
